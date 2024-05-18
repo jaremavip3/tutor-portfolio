7d7a1441da6d4e905d9383125445afe3c49f0868
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2024-05-17 20:14:31 -0400

    refactor: Update Footer component with ID attribute and add personalized message to Hero component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,72 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { use, useEffect, useRef, useState } from "react";
+import { text } from "stream/consumers";
+
 export default function Footer() {
-  return <section id="footer">This is footer</section>;
+  const texts = useRef<Array<SVGTextPathElement | null>>([]);
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end end"],
+  });
+  useEffect(() => {
+    scrollYProgress.on("change", (value) => {
+      texts.current.forEach((text, index) => {
+        text?.setAttribute("startOffset", -35 + index * 35 + value * 40 + "%");
+      });
+    });
+  }, [scrollYProgress]);
+
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    const updateWindowWidth = () => setWindowWidth(window.innerWidth);
+    updateWindowWidth();
+    window.addEventListener("resize", updateWindowWidth);
+    return () => window.removeEventListener("resize", updateWindowWidth);
+  }, []);
+
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [windowWidth < 768 ? -75 : -200, 0]
+  );
+
+  return (
+    <section id="footer" ref={container}>
+      This is footer
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 800 300"
+        className="stroke-[20] md:stroke-[12]"
+      >
+        <path
+          d="M74.37886895989527 93.47715282468295C117.85121787881008 118.53544887570834 197.7510753998672 207.11691420265333 279.7600449547681 211.86280345944857C361.7690145096692 216.6086927162438 365.5761879397942 119.12588298825716 461.8228196129712 115.89867687253451C558.0694512861483 112.67147075681186 676.7585300113274 179.53095971453484 734.4685410485181 196.61616039304232 "
+          id="curve"
+          fill="none"
+          transform="matrix(1.2762815625000004,0,0,1.2762815625000004,-115.71009388486635,-40.61296513797171)"
+        ></path>
+        <text className="text-[28px]  md:text-[20px] font-bold uppercase">
+          {[...Array(3)].map((undefinedValue, index) => {
+            return (
+              <textPath
+                key={index}
+                href="#curve"
+                startOffset={index * 35 + 4 + "%"}
+                // @ts-ignore: Unreachable code error
+                ref={(ref) => (texts.current[index] = ref)}
+              >
+                Ligntning English
+              </textPath>
+            );
+          })}
+        </text>
+      </svg>
+      <motion.div
+        style={{ y }}
+        className="h-16 md:h-[250px]  bg-slate-200 overflow-hidden"
+      ></motion.div>
+    </section>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Lenis from "lenis";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "./components/Header";
@@ -5,10 +7,28 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Features from "./components/Features";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.01,
+      easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+
+      touchMultiplier: 2,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }),
+    [];
   return (
     <main className=" h-dvh flex flex-col ">
       <Header />
@@ -16,6 +36,7 @@ export default function Home() {
       <About />
       <Features />
       <Contact />
+      <Footer />
     </main>
   );
 }
